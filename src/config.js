84ddc7a7ff700e59a2b41e695234101cadde7543
Varun Shah <varun.shah@envestnet.com>
2023-09-13 19:06:28 -0400

    removing config

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 const config = {
-    apiKey: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3Njc0NDdjNTNjODNjNGNiYjNlNTY4OTBjOGVkZDUzYiIsInN1YiI6IjY0ZWY4MWJhZTBjYTdmMDEyZWIxZWE1OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EqmBvZAL5E4ITyLTyVp8TDxv97sO5xJj-yUyjonAV8U',
-    baseURL: 'https://api.themoviedb.org/3',
-    imagesURI: 'https://image.tmdb.org/t/p',
+    apiKey: '', //api key
+    baseURL: '', //base url
+    imagesURI: '', //image url
     imageSize: '/w500',
-    accountId: '20375500',
+    accountId: '', // account id
 };
 
 export default config;
